9df0b8b52d350c9df103c5a4e22ed64cff896c9b
Elisey <eliseya2000@gmail.com>
2024-07-16 17:11:43 +0400

    Added final to userDao field in UserServiceImpl

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -6,7 +6,7 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+    private final UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
         userDao.createUsersTable();
